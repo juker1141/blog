fca2c5afddd62377c45c4e926d8b750964b4a55a
juker1141 <50107434+juker1141@users.noreply.github.com>
2021-03-08 22:22:56 +0800

    update

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,14 @@
-import jsonplaceholder from '../apis/jsonPlaceholder';
+import _ from 'lodash';
+import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = () => async dispatch => {
-  const response = await jsonplaceholder.get('/posts');
+export const fetchPosts = () => async (dispatch) => {
+  const response = await jsonPlaceholder.get('/posts');
 
-  dispatch({ type: 'FETCH_POSTS', payload: response });
+  dispatch({ type: 'FETCH_POSTS', payload: response.data });
+};
+
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: 'FETCH_USER', payload: response.data });
 };
