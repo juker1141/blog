cebbbd47f35dfd4651fbf586171367027db4ba23
juker1141 <50107434+juker1141@users.noreply.github.com>
2021-03-10 00:01:12 +0800

    update

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,10 +1,8 @@
-const userReducer = (state = [], action) => {
+export default (state = [], action) => {
   switch (action.type) {
     case 'FETCH_USER':
       return [...state, action.payload];
     default:
       return state;
-  }
+  };
 };
-
-export default userReducer;
