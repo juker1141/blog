a64660b518c50b399627cd9e90b1b25039f5e984
juker <juker1141@gmail.com>
2021-03-12 17:31:25 +0800

    update

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,11 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPostsAndUsers = () => async (dispatch) => {
-  console.log(123)
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
-  console.log(456)
+
+  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  userIds.forEach((id) => dispatch(fetchUser(id)));
 };
 
 export const fetchPosts = () => async (dispatch) => {
